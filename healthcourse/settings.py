45f4c356644998f2301cbb9dcd040f2127105acd
Musarrat Hussain <musarratpcr@gmail.com>
2024-06-21 09:16:59 +0200

    add SCM_DO_BUILD_DURING_DEPLOYMENT to TRUE

--- healthcourse/settings.py
+++ healthcourse/settings.py
@@ -138,3 +138,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SCM_DO_BUILD_DURING_DEPLOYMENT = True
