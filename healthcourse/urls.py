ef736f0b51d7568a1e5d629e71bc2dd3c8438fcc
Musarrat Hussain <musarratpcr@gmail.com>
2024-07-11 15:57:03 +0200

    set tellme image display

--- healthcourse/urls.py
+++ healthcourse/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
@@ -24,4 +26,4 @@ urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     # path(r'^tellme/', include("tellme.urls")),
     path('tellme/', include("tellme.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
